ab1d956a0f7f7f89060f5325c921904ac57910c3
MUC-StarChild-Mercury <MUC_Mercury@MUC-StarChild-Mercurys-iMac.local>
2014-10-10 23:45:18 +0200

    Preparation of the simple example

--- xcode_tmp/main.cpp
+++ xcode_tmp/main.cpp
@@ -8,8 +8,37 @@
 
 #include <iostream>
 
+class B {
+    int B = 1;
+public:
+    int get();
+};
+
+int B::get()
+{
+    return B;
+}
+
+class A {
+    int A = 0;
+public:
+    int get();
+};
+
+int A::get()
+{
+    return A;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    A a;
+    B b;
+    
+    std::cout << a.get() << "\n";
+    std::cout << b.get() << "\n";
+    
     return 0;
 }
