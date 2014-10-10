95a7c06e74cc478e033630c21cc529a7900f5db7
MUC-StarChild-Mercury <MUC_Mercury@MUC-StarChild-Mercurys-iMac.local>
2014-10-10 22:57:08 +0200

    Initial Commit

---
+++ xcode_tmp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode_tmp
+//
+//  Created by MUC-StarChild-Mercury on 10/10/14.
+//  Copyright (c) 2014 MUC-StarChild-Mercury. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
