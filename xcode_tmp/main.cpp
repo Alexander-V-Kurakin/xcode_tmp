bf4f088827942a7f86730d99ea0fa9d9d8da9ee5
MUC_Mercury <alexander.kurakin@o2online.de>
2015-06-19 13:16:48 +0200

    Exception handling basics

--- xcode_tmp/main.cpp
+++ xcode_tmp/main.cpp
@@ -36,23 +36,48 @@ T I<T>::get_a() const
 
 class Base {
 public:
-    virtual std::string vf() const = 0;
+    Base(int = 0){std::cout << "Base" << std::endl;}
+    
     virtual ~Base(){};
+    
+    virtual std::string vf() const = 0;
 };
 
 class Derived1 : virtual public Base {
 public:
-    std::string vf() const {return "Derived1";};
+    Derived1() : Base(1){}
+    
+    std::string vf() const throw() {return "Derived1";};
 };
 
 class Derived2 : virtual public Base {
 public:
-    std::string vf() const {return "Derived1";};
+    Derived2() : Base(2){}
+    
+    std::string vf() const throw() {return "Derived2";};
 };
 
 class MI : public Derived1, public Derived2 {
 public:
-    std::string vf() const {return Derived1::vf();}
+    MI(){}
+    
+    std::string vf() const throw() {return Derived1::vf();}
+    
+    void ef() throw (const char*) {throw "base class exception";}
+};
+
+class X : public MI {
+public:
+    X(){}
+    
+    std::string vf() const throw() {return Derived2::vf();}
+    
+    void ef(int n)
+    {
+        if (n < 2) throw "const char* exception";
+        else
+            throw n;
+    }
 };
 
 
@@ -61,29 +86,29 @@ int main(int argc, const char * argv[]) {
     char *cp = const_cast<char*>("Hello, C++14 World!\n");
     std::cout << cp << std::endl;
     
-    {
-    int x = 1, y = 2;
-    std::cout << "x = " << x << "\ty = " << y << std::endl;
-    swap(x, y);
-    std::cout << "x = " << x << "\ty = " << y << std::endl;
-    }
-    
-    {
-    float x = 1.1, y = 2.2;
-    std::cout << "x = " << x << "\ty = " << y << std::endl;
-    swap(x, y);
-    std::cout << "x = " << x << "\ty = " << y << std::endl;
-    }
-    std::cout << std::endl;
-    
-    I<float> i(3.3);
-    TRACE(i.get_a());
-    I<int> j(4);
-    TRACE(j.get_a());
-    std::cout << std::endl;
-    
     MI mi;
     TRACE(mi.vf());
+    X x;
+    TRACE(x.vf());
+    TRACE(x.MI::vf());
+    std::cout << std::endl;
+    
+    for (int n = 0; n < 3; n++)
+    {
+        try {
+            x.ef(n);
+        } catch (const char* s) {
+            std::cout << s << " caught\t" << n << std::endl;
+        } catch (...) {
+            std::cout << "any other exception caught\t" << n << std::endl;
+        }
+        
+        try {
+            x.MI::ef();
+        } catch (const char* s) {
+            std::cout << s << " caught\t" << n << std::endl;
+        }
+    }
     std::cout << std::endl;
     
     return 0;
