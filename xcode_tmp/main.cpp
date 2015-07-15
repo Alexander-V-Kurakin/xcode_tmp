35b1de7074bbabbc5df91be4e86dbcb4f4933922
MUC_Mercury <alexander.kurakin@o2online.de>
2015-07-15 12:58:44 +0200

    Global function as class friend

--- xcode_tmp/main.cpp
+++ xcode_tmp/main.cpp
@@ -19,11 +19,16 @@ public:
 };
 
 class Derived3 : public Base1 {
+    int i = 44;
 public:
     void what() const {std::cout << "class Derived3" << std::endl;}
+    
+    friend int ff(Derived3 &);
 };
 
 void f(){throw Derived3();}
+
+int ff(Derived3 &d3){return d3.i;}
 
 
 class Singleton {
@@ -31,20 +36,30 @@ class Singleton {
     Singleton(int x) : i(x){}
     Singleton(const Singleton&);
     int i;
+    static int j;
 public:
     static Singleton* instance(){ return &s; }
     static Singleton& GetHandle(){return s;}
     int GetValue() const {return i;}
+    static int GetValue_j(){return j;}
     void SetValue(int x){i = x;}
 };
 
 Singleton Singleton::s(1);
+int Singleton::j = 0;
 
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     char *cp = const_cast<char*>("Hello, C++14 World!\n");
     std::cout << cp << std::endl;
+    
+    uint64_t a;
+    std::string raw_string = R"(C:\A\B\C\D\file.txt())";
+    
+    std::cout << sizeof a << std::endl;
+    std::cout << sizeof raw_string << "\t" << raw_string << std::endl;
+    std::cout << std::endl;
     
     
     try {
@@ -62,11 +77,16 @@ int main(int argc, const char * argv[]) {
     
     
     Singleton& s = Singleton::GetHandle();
-    std::cout << s.GetValue() << std::endl;
+    std::cout << s.GetValue() << "\t" << s.GetValue_j() << std::endl;
+    
     Singleton& s2 = Singleton::GetHandle();
     s2.SetValue(2);
     std::cout << s.GetValue() << std::endl;
     std::cout << Singleton::instance()->GetValue() << std::endl;
+    std::cout << std::endl;
+    
+    Derived3 d3;
+    TRACE(ff(d3));
     std::cout << std::endl;
     
     return 0;
